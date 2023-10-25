f993ba3bc2f9f5782e94197c65b8b75be6c3ec21
Joao Borges10 <126673387+Joaoborges10@users.noreply.github.com>
2023-10-25 20:30:58 -0300

    implementaçao do botao salvar e integração com o banco

--- app/src/main/java/com/example/cimatec_movie3/adicionarActivity.java
+++ app/src/main/java/com/example/cimatec_movie3/adicionarActivity.java
@@ -1,59 +1,58 @@
 package com.example.cimatec_movie3;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class adicionarActivity extends AppCompatActivity {
     private Button button2;
     private EditText txtNum, txtNome, txtAno, txtMat;
-    private Movie filme;
-    private DatabaseReference reference=FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar);
-        DatabaseReference movies = reference.child("movie");
 
         txtNum = findViewById(R.id.editTxtNum);
-        int nFilme = Integer.parseInt(txtNum.getText().toString());
-
         txtAno = findViewById(R.id.editTxtAno);
         txtNome = findViewById(R.id.editTxtNome);
         txtMat = findViewById(R.id.editTxtMat);
 
-        button2= findViewById(R.id.button2);
+        button2 = findViewById(R.id.button2);
+
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                filme.setAno(Integer.parseInt(txtAno.getText().toString()));
-                filme.setNome(txtNome.getText().toString());
-                filme.setCurtida(0);
-                String matricula = txtMat.getText().toString();
+                String numText = txtNum.getText().toString();
+                if (!numText.isEmpty()) {
+                    int nFilme = Integer.parseInt(numText);
+                    DatabaseReference movies = reference.child("movie");
+                    String matricula = txtMat.getText().toString();
 
-                switch (nFilme){
-                    case 1:
-                        movies.child(matricula).child("filme1").setValue(filme);
-                        break;
-                    case 2:
-                        movies.child(matricula).child("filme2").setValue(filme);
-                        break;
-                    case 3:
-                        movies.child(matricula).child("filme3").setValue(filme);
-                        break;
-                    case 4:
-                        movies.child(matricula).child("filme4").setValue(filme);
-                        break;
-                    default:
-                        movies.child(matricula).child("filme4").setValue(filme);
-                        break;
+                    // Consulta o banco de dados para obter o número do próximo filme
+                    movies.child(matricula).get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+                        @Override
+                        public void onSuccess(DataSnapshot dataSnapshot) {
+                            long numFilmes = dataSnapshot.getChildrenCount();
+                            String nomeFilme = "filme" + (numFilmes + 1);
+                            Movie filme = new Movie();
+                            filme.setAno(Integer.parseInt(txtAno.getText().toString()));
+                            filme.setNome(txtNome.getText().toString());
+                            filme.setCurtida(0);
+
+                            // Salva o filme com o novo nome
+                            movies.child(matricula).child(nomeFilme).setValue(filme);
+                        }
+                    });
+                } else {
+                    // Trate o caso em que o campo está vazio, talvez exibindo uma mensagem de erro ao usuário.
                 }
             }
         });
